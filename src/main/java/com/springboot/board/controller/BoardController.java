7c7050a6e38d9ab6c82ebfb3397a5f5942d7a0d7
tlsrja100 <tlsrja10@naver.com>
2021-11-30 21:46:01 +0900

    layout 구성

--- src/main/java/com/springboot/board/controller/BoardController.java
+++ src/main/java/com/springboot/board/controller/BoardController.java
@@ -2,12 +2,14 @@ package com.springboot.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 
-    @GetMapping("/")
-    public String getboardList() {
-        return "board/boardList.html";
+    @GetMapping("/list")
+    public String list() {
+        return "board/list";
     }
 }
